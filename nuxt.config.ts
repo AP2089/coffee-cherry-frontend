cfc3305fda9f151e77b19e025410da45af1f2c06
Andrey Proskurin <mr.ap2089@gmail.com>
2026-09-01 10:25:29 +0300

    Обновляет сборку

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,10 +57,10 @@ export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css', '~/assets/scss/main.scss'],
 
   runtimeConfig: {
-    apiUrl: process.env.API_URL || '',
+    apiUrl: '',
     public: {
-      apiUrl: process.env.API_URL || '',
-      socketUrl: process.env.SOCKET_URL || '',
+      apiUrl: '',
+      socketUrl: '',
     },
   },
 
